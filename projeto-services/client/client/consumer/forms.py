4958dfb74705b30b1fd22a90762216cc0bc4329b
BrunoTumelero <Bruno.Tumelero@outlook.com>
2023-02-03 22:28:16 -0300

    create models and endpoint for avaliation product

--- projeto-services/client/client/consumer/forms.py
+++ projeto-services/client/client/consumer/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from client.register.models import ConsumersCards, Whishes
+from client.register.models import ConsumersCards, Whishes, ProductsRating
 
 class ConsumersCardsForm(forms.ModelForm):
 
@@ -13,3 +13,9 @@ class WhishesForm(forms.ModelForm):
     class Meta:
         model = Whishes
         fields = ['name_whishes_list', 'consumer', 'product', 'annotation', 'priority', 'amount']
+
+class ProductsRatingForm(forms.ModelForm):
+
+    class Meta:
+        model = ProductsRating
+        fields = ['user', 'product', 'rating']
