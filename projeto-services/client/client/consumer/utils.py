fafa0ec2909fdec2ffd3c385b659f31126a69409
BrunoTumelero <Bruno.Tumelero@outlook.com>
2023-02-06 22:35:01 -0300

    gerate token, headers and key pix

--- projeto-services/client/client/consumer/utils.py
+++ projeto-services/client/client/consumer/utils.py
@@ -56,12 +56,6 @@ def _headers():
 
 def generate_key_pix():
     print('Gerando chave...')
-    credenciais = {
-            'client_id': 'Client_Id_ec3f3b7a718656f4cafe2d66a65e096558520d27',
-            'client_secret': 'Client_Secret_2f025a925ade7cf5bde4afc878fb692fd26134f8',
-            'sandbox': True,
-            'certificate': 'Projeto_dev\projeto-services\client\client\credinciais\dev.pem'
-        }
 
     url = "https://api-pix-h.gerencianet.com.br/v2/gn/evp"
     certificado = f'client/credinciais/{settings.CERT_DEV}'
