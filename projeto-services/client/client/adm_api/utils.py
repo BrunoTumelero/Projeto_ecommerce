b4d95610dd8f894ce87b3bd067655b2c06c2c2d6
BrunoTumelero <Bruno.Tumelero@outlook.com>
2023-03-12 17:38:11 -0300

    Create staff user

---
+++ projeto-services/client/client/adm_api/utils.py
@@ -0,0 +1,22 @@
+import hashlib
+import time
+import random
+import string
+
+from client.register.models import *
+
+def _create_token(user):
+    random.seed()
+    cod = hashlib.md5()
+    cod.update((str(random.randint(0, 100000000000)) +
+                str(time.time())).encode("utf-8"))
+    session_token = cod.hexdigest()
+    user_session = UserSession(user=user, session_token=session_token)
+    user_session.save()
+    return session_token
+
+def _create_activation_token():
+    cod = string.ascii_letters + string.digits
+    len_cod = random.randint(5, 20)
+    key = ''.join(random.choice(cod) for _ in range(len_cod))
+    return str(key)
--- projeto-services/client/client/adm_api/views.py
+++ projeto-services/client/client/adm_api/views.py
@@ -1,7 +1,9 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.db import IntegrityError
 
 from .decorators import staff_authenticate
+from .utils import _create_token, _create_activation_token
 from client.register.models import State, City, ProductCategory, User, CompanySpecialty, SubCategory, Permission, CompanyPermission
 from .forms import StateForm, CityForm, CompanySpecialtyForm, ProductCategoryForm, ProductSubCategoryForm, PermissionForm, CompanyPermissionForm
 
@@ -210,3 +212,43 @@ def create_company_permissions(request):
             return JsonResponse({'message': 'Permissões alteradas com sucesso', 'status': 200})
         
     return JsonResponse({'message': 'ERRO', 'status': 400})
+
+@csrf_exempt
+def create_staff(request):
+    user_email = request.POST.get('user_email', None)
+    password = request.POST.get('password', None)
+    type_user = request.POST.get('type_user', None)
+    
+    if user_email and password:
+        #Check email already exists
+        if User.objects.filter(email=user_email).exists():
+            try:
+                user = User.objects.get(email=user_email)
+            except TypeError as e:
+                print(e)
+            if user.is_company:
+                return JsonResponse({'message': 'Email já cadastrado', 'status': 400})
+            else:
+                user = User.objects.get(email=user_email)
+                user.is_staff = True
+                user.save()
+                return JsonResponse({'message': 'Usuário alterado com sucesso', 'status': 200})
+        else:
+            if type_user == 'staff':
+                try:
+                    user = User.objects.create(email=user_email)
+                    user.email = user_email
+                    user.set_password(password)
+                    user.is_active = True
+                    user.is_staff = True
+                    user.activation_key = _create_activation_token()
+                    session_token = _create_token(user)
+                    user.save()
+                    
+                    return JsonResponse({'message': 'Usuário cadastrado com sucesso', 'id': user.id, '_stafftoken': session_token, 'status': 200})
+                except IntegrityError:
+                    #email duplicate
+                    return JsonResponse({'message': 'Emais já cadastrado', 'status': 400})
+            else:
+                return JsonResponse({'message': 'Tipo inválido', 'status': 400})
+    return JsonResponse({'message': 'Informe os campos obrigatórios', 'status': 400})
